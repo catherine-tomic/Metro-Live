36c91a7707543a776d1c11b3ff06eecb76d19c72
catherine-tomic <cattomic@gmail.com>
2025-10-16 22:33:22 -0400

    clsx installed & file structure created

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import clsx from "clsx";
 
 const App = () => {
+  const classes = clsx(
+    "Container bg-metro-silver text-metro-yellow"
+  )
+
   return (
     <div>
-      <div className="Container bg-red-500">
+      <div className={classes}>
         <p>Hello World</p>
       </div>
     </div>
